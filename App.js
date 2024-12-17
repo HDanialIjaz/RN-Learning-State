e1dc66c1bc40c8d6f04fb3763c8536ba80eba764
Hafiz Muhammad Danial <danialijaz098@gmail.com>
2024-12-17 15:17:53 +0500

    feat: add button to change color

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+
 
 export default function App() {
+  let color = [ 'green', 'blue', 'yellow', 'pink', 'black', 'purple', 'orange', 'brown', 'grey'];
+
+  function prevIndex  (index) {
+
+    return (index + 1) % color.length;
+  
+  };
+  const [bgcolor, setBgcolor] = useState('red');
+  const [index, setIndex] = useState(0);
+  const changecolor = () => {
+    setBgcolor(color[index])
+    setIndex(prevIndex(index));
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor: bgcolor }]}>
+      <TouchableOpacity onPress={changecolor}>
+        <Text style={styles.text}>Change Color</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ 
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'white',
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
 });
+
+
